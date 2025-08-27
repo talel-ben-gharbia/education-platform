b3186375314021200211f5c691d2625533c9690e
talel ben gharbia <talelbengharbia213@gmail.com>
2025-08-27 13:14:56 +0100

    second commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react'
+import logo from '../../assets/logo.png'
+import '../Navbar/Navbar.css'
+function Navbar() {
+  const [sticky,setSticky] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener('scroll', ()=>{
+      window.scrollY > 50 ? setSticky(true) : setSticky(false)
+    })
+  },[]) ;
+  return (
+    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
+        <img src={logo} alt="" className='logo' />
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li><button className='btn'>Contact us</button> </li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
